1af31f1d7b4403aace209d3e177631ff84e68986
Hero Protagonist <heroprotagonist32@gmail.com>
2025-10-25 12:26:12 -0300

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Heisig Remembering the Kanji mdict
+
+`mdict -a heisig.txt heisig.mdx`
+
+Check releases
+
+thanks to [John Graham](https://gitlab.com/johngavingraham/yomikanji/) for providing the .csv data
---
+++ main.ts
@@ -0,0 +1,32 @@
+import { csvParse } from "npm:d3";
+
+const csv = Deno.readTextFileSync("./KANJI_RTK_INDEX.csv");
+
+const arr = csvParse(csv) as {
+  kanji: string;
+  id_5th_ed: string;
+  id_6th_ed: string;
+  keyword_5th_ed: string;
+  keyword_6th_ed: string;
+  components: string;
+  on_reading: string;
+  kun_reading: string;
+}[];
+
+const html = arr.map((el) => {
+  return (
+    `<b>${el.kanji}</b></br>
+    <span>keyword: ${el.keyword_5th_ed} ${
+      el.keyword_5th_ed !== el.keyword_6th_ed ? ` k6: ${el.keyword_6th_ed}` : ""
+    }</span></br>
+    ${el.components ? `<span>components: ${el.components}</span></br>` : ''}
+    ${el.on_reading ? `<span>on:${el.on_reading}</span></br>` : ''}
+    ${el.kun_reading ? `<span>kun:${el.kun_reading}</span></br>`: ''}
+    <span>id: ${el.id_5th_ed} ${
+      el.id_5th_ed !== el.id_6th_ed ? ` id6: ${el.id_6th_ed}` : ""
+    }</span></br>
+    `
+  );
+}).join("\n</>\n");
+
+Deno.writeTextFileSync("./heisig.txt", html);
